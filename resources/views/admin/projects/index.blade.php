83d01d056fdc3afa716f8cb2540f57c15fcc1372
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-23 20:00:16 +0200

    fix destroy on projects table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -54,20 +54,20 @@
                                 </a>
                                 <!-- Modal trigger button -->
                                 <button type="button" class="btn btn-danger btn-sm m-2" data-bs-toggle="modal"
-                                    data-bs-target="#modalId-delete">
+                                    data-bs-target="#modalId-{{ $project->id }}">
                                     <i class="fa-solid fa-trash"></i>
                                 </button>
 
                                 <!-- Modal Body -->
                                 <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                                <div class="modal fade" id="modalId-delete" tabindex="-1" data-bs-backdrop="static"
-                                    data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId"
-                                    aria-hidden="true">
+                                <div class="modal fade" id="modalId-{{ $project->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTitle-{{ $project->id }}" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
                                         role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="modalTitleId">
+                                                <h5 class="modal-title" id="modalTitle-{{ $project->id }}">
                                                     DELETING PROJECT
                                                 </h5>
                                             </div>
